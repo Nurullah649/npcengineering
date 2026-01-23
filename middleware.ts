456d711905579ba1d7471584d1561380ac772272
Nurullah649 <113285824+Nurullah649@users.noreply.github.com>
2026-01-23 17:17:02 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -65,6 +65,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        '/admin/:path*',
+        // '/admin/:path*', // Middleware kontrolünü geçici olarak devre dışı bırakıyoruz
     ],
 }
